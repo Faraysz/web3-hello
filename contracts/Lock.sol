c12f5275cad42848dabceafbbbefc45623d90ccf
Faraytodi Atmajaya <150215021+Faraysz@users.noreply.github.com>
2025-07-25 20:56:57 +0700

    Package npm, node, and hardat

---
+++ contracts/Lock.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract HelloWeb3 {
+    string public message = "Halo Dunia Web3!";
+
+    function setMessage(string calldata _msg) public {
+        message = _msg;
+    }
+}
